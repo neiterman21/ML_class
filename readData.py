92589412f904c308b34cb111e4601e47832246d8
neiterman21 <neiterman21@gmail.com>
2020-12-23 17:37:25 +0200

    working adaboost class. need to fix line rules

--- readData.py
+++ readData.py
@@ -1,27 +1,28 @@
 import pandas as pd
+import numpy as np
 
 
 def iris_data(file):
-    iris = pd.DataFrame(
-        data=pd.read_csv(file, engine="python", names=["c1", "c2", "c3", "c4", "label"])
-    )
-    iris = iris[iris["label"] != "Iris-setosa"]
-    iris.loc[iris["label"] == "Iris-versicolor", "label"] = 1
-    iris.loc[iris["label"] == "Iris-virginica", "label"] = -1
+    iris = pd.DataFrame(data=pd.read_csv(file , engine='python'))
+    iris = iris.iloc[:]
+
+    iris.columns = ['c1', 'c2', 'c3', 'c4', 'label']
+    iris = iris[iris['label'] != 'Iris-setosa']
+    iris.loc[iris['label'] == 'Iris-versicolor', 'label'] = 1
+    iris.loc[iris['label'] == 'Iris-virginica', 'label'] = -1
+
     return iris
 
 
 def hbt_data(file):
-    hbt = pd.DataFrame(
-        data=pd.read_csv(
-            file, delimiter=r"\s+", engine="python", names=["c1", "label", "c2"]
-        )
-    )
-    hbt = hbt[hbt.columns[[0, 2, 1]]]
-    hbt.loc[hbt["label"] == 2, "label"] = -1
+
+    hbt = pd.DataFrame(data=pd.read_csv(file, delimiter=r"\s+", engine='python'))
+    # hbt = hbt.iloc[:, [0, 2]] //if we want first and third columns
+    hbt.columns = ['c1', 'label', 'c2']
+    #values of 1, -1
+# reorder columns so that label is the third column
+    hbt = hbt[hbt.columns[[0,2,1]]]
+
+    # change label 2 --> -1
+    hbt.loc[hbt['label'] == 2, 'label'] = -1
     return hbt
-
-
-if __name__ == "__main__":
-    print(iris_data("iris.data").shape)
-    print(hbt_data("HC_Body_Temperature.txt").shape)
